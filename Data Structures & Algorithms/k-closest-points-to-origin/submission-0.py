9c1eda220ea22f79e79d2b6df469f4079ed328a5
Nathan03Nguyen <34860301+Nathan03Nguyen@users.noreply.github.com>
2026-09-07 13:54:52 -0700

    Update: k-closest-points-to-origin - submission-0

--- Data Structures & Algorithms/k-closest-points-to-origin/submission-0.py
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-0.py
@@ -4,11 +4,11 @@ class Solution:
         for x, y in points:
             dist = (x ** 2) + (y ** 2)
             min_heap.append([dist, x, y])
-
         heapq.heapify(min_heap)
+
         res = []
         while k > 0:
             dist, x, y = heapq.heappop(min_heap)
-            res.append([x, y])
+            res.append([x,y])
             k -= 1
         return res
