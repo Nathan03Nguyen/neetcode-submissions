f073fd8f405de2d64059a27348d9be1d99b5cc56
Nathan03Nguyen <34860301+Nathan03Nguyen@users.noreply.github.com>
2026-09-10 13:02:31 -0700

    Update: subsets - submission-1

--- Data Structures & Algorithms/subsets/submission-1.py
+++ Data Structures & Algorithms/subsets/submission-1.py
@@ -1,18 +1,19 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        subset = []
+        # Time : O(2^n)
+        # Space : O(n)
+        res, sol = [], []
 
-        def dfs(i):
-            if i >= len(nums):
-                res.append(subset.copy())
+        def backtrack(i):
+            if i == len(nums):
+                res.append(sol[:])
                 return
-            
-            subset.append(nums[i])
-            dfs(i + 1)
 
-            subset.pop()
-            dfs(i + 1)
+            backtrack(i + 1)
 
-        dfs(0)
+            sol.append(nums[i])
+            backtrack(i + 1)
+            sol.pop()
+
+        backtrack(0)
         return res
