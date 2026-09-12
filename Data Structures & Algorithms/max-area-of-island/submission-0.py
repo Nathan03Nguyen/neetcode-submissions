e7a5195f022ad05c60045b05254ffdf72711342d
Nathan03Nguyen <34860301+Nathan03Nguyen@users.noreply.github.com>
2026-09-12 16:22:13 -0700

    Update: max-area-of-island - submission-0

--- Data Structures & Algorithms/max-area-of-island/submission-0.py
+++ Data Structures & Algorithms/max-area-of-island/submission-0.py
@@ -7,7 +7,7 @@ class Solution:
             if (r < 0 or r == rows or c < 0 or c == cols or
                 grid[r][c] == 0 or (r, c) in visited):
                 return 0
-            visited.add((r, c))
+            visited.add((r,c))
             return (1 + dfs(r + 1, c) +
                         dfs(r - 1, c) +
                         dfs(r, c + 1) +
