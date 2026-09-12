9adeac2bb6d7563388fd92d54db38838cfe90f55
Nathan03Nguyen <34860301+Nathan03Nguyen@users.noreply.github.com>
2026-09-12 16:05:37 -0700

    Update: count-number-of-islands - submission-0

--- Data Structures & Algorithms/count-number-of-islands/submission-0.py
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.py
@@ -2,7 +2,6 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
-
         rows, cols = len(grid), len(grid[0])
         visited = set()
         islands = 0
@@ -15,16 +14,13 @@ class Solution:
             while q:
                 row, col = q.popleft()
                 directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-
                 for dr, dc in directions:
                     r, c = row + dr, col + dc
-                    if (r in range(rows) and
-                        c in range(cols) and
-                        grid[r][c] == '1' and
-                        (r, c) not in visited):
+                    if (r in range(rows) and c in range(cols) and
+                        grid[r][c] == '1' and (r,c) not in visited):
+                        visited.add((r,c))
                         q.append((r, c))
-                        visited.add((r, c))
-
+        
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == '1' and (r, c) not in visited:
