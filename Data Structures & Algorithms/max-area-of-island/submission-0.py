c4cc92280319129c1f7dd79d087a0e953fcacb59
Nathan03Nguyen <34860301+Nathan03Nguyen@users.noreply.github.com>
2026-09-12 12:21:43 -0700

    Update: max-area-of-island - submission-0

--- Data Structures & Algorithms/max-area-of-island/submission-0.py
+++ Data Structures & Algorithms/max-area-of-island/submission-0.py
@@ -4,10 +4,10 @@ class Solution:
         visited = set()
 
         def dfs(r, c):
-            if (r < 0 or r == rows or c < 0 or c == cols or 
+            if (r < 0 or r == rows or c < 0 or c == cols or
                 grid[r][c] == 0 or (r, c) in visited):
                 return 0
-            visited.add((r,c))
+            visited.add((r, c))
             return (1 + dfs(r + 1, c) +
                         dfs(r - 1, c) +
                         dfs(r, c + 1) +
@@ -15,5 +15,5 @@ class Solution:
         area = 0
         for r in range(rows):
             for c in range(cols):
-                area = max(area, dfs(r,c))
+                area = max(area, dfs(r, c))
         return area
