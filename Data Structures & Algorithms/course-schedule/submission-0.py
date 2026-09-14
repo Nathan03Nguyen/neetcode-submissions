ffaff9ee119a8d7cd42db27f12f5702edd0c58a9
Nathan03Nguyen <34860301+Nathan03Nguyen@users.noreply.github.com>
2026-09-14 11:40:34 -0700

    Update: course-schedule - submission-0

--- Data Structures & Algorithms/course-schedule/submission-0.py
+++ Data Structures & Algorithms/course-schedule/submission-0.py
@@ -1,14 +1,12 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         g = defaultdict(list)
-        courses = prerequisites
-        for a, b in courses:
+        for a, b in prerequisites:
             g[a].append(b)
         
         unvisited = 0
         visiting = 1
         visited = 2
-
         states = [unvisited] * numCourses
 
         def dfs(node):
@@ -17,7 +15,7 @@ class Solution:
                 return True
             elif state == visiting:
                 return False
-
+            
             states[node] = visiting
 
             for nei in g[node]:
@@ -25,7 +23,7 @@ class Solution:
                     return False
             states[node] = visited
             return True
-
+        
         for i in range(numCourses):
             if not dfs(i):
                 return False
