7459ed24fb47b9d9df3560e5a8f4ee974e934670
Nathan03Nguyen <34860301+Nathan03Nguyen@users.noreply.github.com>
2026-09-14 11:51:41 -0700

    Update: surrounded-regions - submission-1

--- Data Structures & Algorithms/surrounded-regions/submission-1.py
+++ Data Structures & Algorithms/surrounded-regions/submission-1.py
@@ -11,13 +11,13 @@ class Solution:
             capture(r - 1, c)
             capture(r, c + 1)
             capture(r, c - 1)
-
+        
         for r in range(rows):
             for c in range(cols):
                 if (board[r][c] == 'O' and
                     (r in [0, rows - 1] or c in [0, cols - 1])):
                     capture(r, c)
-        
+
         for r in range(rows):
             for c in range(cols):
                 if board[r][c] == 'O':
