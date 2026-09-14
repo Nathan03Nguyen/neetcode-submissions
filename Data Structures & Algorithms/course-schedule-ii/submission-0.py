0f87b06a8963b90bcee39ec93f5bf6dd129f0306
Nathan03Nguyen <34860301+Nathan03Nguyen@users.noreply.github.com>
2026-09-14 14:09:09 -0700

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        order = []
+        g = defaultdict(list)
+        for a, b in prerequisites:
+            g[a].append(b)
+        
+        unvisited = 0
+        visiting = 1
+        visited = 2
+
+        states = [unvisited] * numCourses
+
+        def dfs(node):
+            if states[node] == visiting:
+                return False
+            elif states[node] == visited:
+                return True
+            states[node] = visiting
+
+            for nei in g[node]:
+                if not dfs(nei):
+                    return False
+            states[node] = visited
+            order.append(node)
+            return True
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return []
+        return order
