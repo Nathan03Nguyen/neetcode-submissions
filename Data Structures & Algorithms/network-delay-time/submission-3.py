6489a93a4f86479dff06a87bd393186301b77b90
Nathan03Nguyen <34860301+Nathan03Nguyen@users.noreply.github.com>
2026-09-15 20:12:35 -0700

    Update: network-delay-time - submission-3

--- Data Structures & Algorithms/network-delay-time/submission-3.py
+++ Data Structures & Algorithms/network-delay-time/submission-3.py
@@ -3,18 +3,19 @@ class Solution:
         edges = defaultdict(list)
         for u, v, w in times:
             edges[u].append((v, w))
-
+        
         min_heap = [(0, k)]
         visited = set()
-        res = 0
+        t = 0
+
         while min_heap:
             w1, n1 = heapq.heappop(min_heap)
             if n1 in visited:
                 continue
             visited.add(n1)
-            res = max(res, w1)
+            t = max(t, w1)
 
             for n2, w2 in edges[n1]:
                 if n2 not in visited:
                     heapq.heappush(min_heap, (w1 + w2, n2))
-        return res if len(visited) == n else -1
+        return t if len(visited) == n else -1
