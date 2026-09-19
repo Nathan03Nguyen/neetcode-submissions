b39f0efa05629813d712894ce43a2d2415f13242
Nathan03Nguyen <34860301+Nathan03Nguyen@users.noreply.github.com>
2026-09-19 12:31:08 -0700

    Update: longest-increasing-subsequence - submission-0

--- Data Structures & Algorithms/longest-increasing-subsequence/submission-0.py
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-0.py
@@ -5,5 +5,5 @@ class Solution:
         for i in range(len(nums) - 1, -1, -1):
             for j in range(i + 1, len(nums)):
                 if nums[i] < nums[j]:
-                    lis[i] = max(lis[i], 1 + lis[j])
+                    lis[i] = max(lis[i], lis[j] + 1)
         return max(lis)
