7e15bf178a9f7f650d2b21987ebd7350e2a5d471
Nathan03Nguyen <34860301+Nathan03Nguyen@users.noreply.github.com>
2026-09-21 11:18:58 -0700

    Update: target-sum - submission-0

--- Data Structures & Algorithms/target-sum/submission-0.py
+++ Data Structures & Algorithms/target-sum/submission-0.py
@@ -4,10 +4,10 @@ class Solution:
 
         def backtrack(i, total):
             if i == len(nums):
-                return 1 if target == total else 0
+                return 1 if total == target else 0
             if (i, total) in dp:
                 return dp[(i, total)]
-            
+
             dp[(i, total)] = (backtrack(i + 1, total + nums[i]) +
                               backtrack(i + 1, total - nums[i]))
             return dp[(i, total)]
