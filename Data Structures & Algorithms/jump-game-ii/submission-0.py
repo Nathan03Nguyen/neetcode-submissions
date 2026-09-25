c8114c18b837771997d5e7deaa994615272f71c0
Nathan03Nguyen <34860301+Nathan03Nguyen@users.noreply.github.com>
2026-09-24 21:21:27 -0700

    Update: jump-game-ii - submission-0

--- Data Structures & Algorithms/jump-game-ii/submission-0.py
+++ Data Structures & Algorithms/jump-game-ii/submission-0.py
@@ -7,6 +7,7 @@ class Solution:
             farthest = 0
             for i in range(l, r + 1):
                 farthest = max(farthest, i + nums[i])
-            l, r = r + 1, farthest
+            l = r + 1
+            r = farthest
             res += 1
         return res
