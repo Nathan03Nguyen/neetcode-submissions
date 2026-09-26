fe72317108cf63efa2e6563bcbbdcfbec2dcc38d
Nathan03Nguyen <34860301+Nathan03Nguyen@users.noreply.github.com>
2026-09-26 12:08:03 -0700

    Update: gas-station - submission-0

--- Data Structures & Algorithms/gas-station/submission-0.py
+++ Data Structures & Algorithms/gas-station/submission-0.py
@@ -6,7 +6,6 @@ class Solution:
         total = res = 0
         for i in range(len(gas)):
             total += (gas[i] - cost[i])
-            
             if total < 0:
                 total = 0
                 res = i + 1
